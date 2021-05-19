0ac6889a8a1410e6e572ed6569bf12cc604a93b8
Marcus Berggren <marcus.berggren@outlook.com>
2021-05-19 19:59:48 -0400

    refactored client controller

--- lib/routes/ClientRoutes.ts
+++ lib/routes/ClientRoutes.ts
@@ -3,6 +3,7 @@ import * as Debug from "debug";
 import { IApplication } from "../app";
 const debug = Debug("AuthServer:ClientRoutes:");
 import Db from "../db/db";
+import { clientController } from "../controllers/ClientController";
 
 export class ClientRoutes {
 
@@ -10,17 +11,7 @@ export class ClientRoutes {
         let db: Db = app.Db;
 
         app.post("/client/create", async (req: Request, res: Response) => {
-            debug(`Adding Client: ${JSON.stringify(req.body)}`);
-            let client = await db.addClient(req?.body?.clientId, req?.body?.clientSecret, req?.body?.redirectUris,
-                                          req?.body?.scope, req?.body?.public);
-            debug(`Sending client: ${JSON.stringify(client)}`);
-
-            if (client) {
-                client.clientSecret = "";
-                res.status(200).send(client);
-            } else {
-                res.status(500).send("Internal error.");
-            }
+            clientController.addClient(req, res, db);
         });
     }
 }
