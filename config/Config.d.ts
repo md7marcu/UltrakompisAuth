953301afd8ca88a12b50e28c7811b324349df27e
Marcus Berggren <marcus.berggren@outlook.com>
2021-07-27 21:51:46 -0400

    fixed vulnerable packages

--- config/Config.d.ts
+++ config/Config.d.ts
@@ -5,6 +5,22 @@ declare module "node-config-ts" {
     settings: Settings
     wellKnown: WellKnown
   }
+  interface WellKnown {
+    issuer: string
+    authorization_endpoint: string
+    device_authorization_endpoint: string
+    token_endpoint: string
+    userinfo_endpoint: string
+    revocation_endpoint: string
+    jwks_uri: string
+    response_types_supported: string[]
+    subject_types_supported: string[]
+    scopes_supported: string[]
+    token_endpoint_auth_methods_supported: string[]
+    claims_supported: string[]
+    code_challenge_methods_supported: string[]
+    grant_types_supported: string[]
+  }
   interface Settings {
     issuer: string
     audience: string
@@ -60,22 +76,6 @@ declare module "node-config-ts" {
     redirectUris: string[]
     scope: string[]
     public?: boolean
-  }
-
-  interface WellKnown {
-    issuer: string
-    authorization_endpoint: string
-    token_endpoint: string
-    userinfo_endpoint: string
-    revocation_endpoint: string
-    jwks_uri: string
-    response_types_supported: string[]
-    subject_types_supported: string[]
-    scopes_supported: string[]
-    token_endpoint_auth_methods_supported: string[]
-    claims_supported: string[]
-    code_challenge_methods_supported: string[]
-    grant_types_supported: string[]
   }
   export const config: Config
   export type Config = IConfig
